596a319b20f82ac7319de45079e3a86df7ce486a
Arun Rathore <arathore@nowoptics.com>
2024-03-25 14:21:56 +0530

    Initial Commit

---
+++ Audio Visualizer/Audio_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_VisualizerApp.swift
+//  Audio Visualizer
+//
+//  Created by Arun Rathore on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Audio_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
